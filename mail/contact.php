6e7a7228abc64571098c79cf5d048a10739c9814
ngkjessica <148161386+ngkjessica@users.noreply.github.com>
2023-10-21 23:20:54 +0530

    Update contact.php

--- mail/contact.php
+++ mail/contact.php
@@ -1,20 +1,23 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+if (empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    echo "error"; // Send an error response
+    exit();
 }
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$name = strip_tags(htmlspecialchars($_POST['name']);
+$email = strip_tags(htmlspecialchars($_POST['email']);
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']);
+$message = strip_tags(htmlspecialchars($_POST['message']);
 
-$to = "info@example.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$to = "soham1255@gmail.com";
+$subject = "$m_subject: $name";
+$body = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: $name\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
-$header .= "Reply-To: $email";	
+$header .= "Reply-To: $email";
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+if (mail($to, $subject, $body, $header)) {
+    echo "success"; // Send a success response
+} else {
+    echo "error"; // Send an error response
+}
 ?>
