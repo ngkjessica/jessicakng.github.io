8a7622ee2b5d2a6b0d40c3653bd70974c1b686c3
ngkjessica <148161386+ngkjessica@users.noreply.github.com>
2023-10-21 23:20:08 +0530

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -1,65 +1,28 @@
 $(function () {
+    $("#contactForm").submit(function (event) {
+        event.preventDefault();
 
-    $("#contactForm").submit(function(){
-        preventSubmit: true,
-        submitError: function ($form, event, errors) {
-        },
-        submitSuccess: function ($form, event) {
-            event.preventDefault();
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var subject = $("input#subject").val();
-            var message = $("textarea#message").val();
+        var $form = $(this);
 
-            $this = $("#sendMessageButton");
-            $this.prop("disabled", true);
-
-            $.ajax({
-                url: "contact.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    subject: subject,
-                    message: message
-                },
-                cache: false,
-                success: function () {
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                            .append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false);
-                    }, 1000);
+        $.ajax({
+            url: "contact.php",
+            type: "POST",
+            data: $form.serialize(), // Serialize form data
+            success: function (response) {
+                if (response === "success") {
+                    $('#success').html('<div class="alert alert-success">Your message has been sent.</div>');
+                    $form.trigger("reset");
+                } else {
+                    $('#success').html('<div class="alert alert-danger">Sorry, there was an error. Please try again later.</div>');
                 }
-            });
-        },
-        filter: function () {
-            return $(this).is(":visible");
-        },
+            },
+            error: function () {
+                $('#success').html('<div class="alert alert-danger">Sorry, there was an error. Please try again later.</div>');
+            }
+        });
     });
 
-    $("a[data-toggle=\"tab\"]").click(function (e) {
-        e.preventDefault();
-        $(this).tab("show");
+    $('#name').focus(function () {
+        $('#success').html('');
     });
-});
-
-$('#name').focus(function () {
-    $('#success').html('');
 });
