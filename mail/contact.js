a770a58e7f261481d6e0b04bf251c42021bd5f91
ngkjessica <148161386+ngkjessica@users.noreply.github.com>
2023-10-21 23:43:19 +0530

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -1,28 +1,28 @@
-$(function () {
-    $("#contactForm").submit(function (event) {
-        event.preventDefault();
+// $(function () {
+//     $("#contactForm").submit(function (event) {
+//         event.preventDefault();
 
-        var $form = $(this);
+//         var $form = $(this);
 
-        $.ajax({
-            url: "contact.php",
-            type: "POST",
-            data: $form.serialize(), // Serialize form data
-            success: function (response) {
-                if (response === "success") {
-                    $('#success').html('<div class="alert alert-success">Your message has been sent.</div>');
-                    $form.trigger("reset");
-                } else {
-                    $('#success').html('<div class="alert alert-danger">Sorry, there was an error. Please try again later.</div>');
-                }
-            },
-            error: function () {
-                $('#success').html('<div class="alert alert-danger">Sorry, there was an error. Please try again later.</div>');
-            }
-        });
-    });
+//         $.ajax({
+//             url: "contact.php",
+//             type: "POST",
+//             data: $form.serialize(), // Serialize form data
+//             success: function (response) {
+//                 if (response === "success") {
+//                     $('#success').html('<div class="alert alert-success">Your message has been sent.</div>');
+//                     $form.trigger("reset");
+//                 } else {
+//                     $('#success').html('<div class="alert alert-danger">Sorry, there was an error. Please try again later.</div>');
+//                 }
+//             },
+//             error: function () {
+//                 $('#success').html('<div class="alert alert-danger">Sorry, there was an error. Please try again later.</div>');
+//             }
+//         });
+//     });
 
-    $('#name').focus(function () {
-        $('#success').html('');
-    });
-});
+//     $('#name').focus(function () {
+//         $('#success').html('');
+//     });
+// });
